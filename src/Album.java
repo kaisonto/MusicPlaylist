62be5710e0e8dbe3594552453c884cc0292c1bb4
kaisonto <hokmanto@hotmail.com>
2018-07-15 14:26:16 +0200

    add inner class

--- src/Album.java
+++ src/Album.java
@@ -4,48 +4,67 @@ import java.util.LinkedList;
 public class Album {
     private String album;
     private String artist;
-    private ArrayList<Song> songs;
+//    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String album, String artist) {
         this.album = album;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+//        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
 
-    private Song findSong(String songTitle){
-        for(Song checkedSong: this.songs){
-            if(checkedSong.getSongTitle() == songTitle){
-                return checkedSong;
-            }
-        }
-        return  null;
-    }
+//    private Song findSong(String songTitle){
+//        for(Song checkedSong: this.songs){
+//            if(checkedSong.getSongTitle() == songTitle){
+//                return checkedSong;
+//            }
+//        }
+//        return  null;
+//    }
 
     public boolean addSong(String songTitle, double songLength){
+    return this.songs.add(new Song(songTitle,songLength));
+/*
     if (findSong(songTitle) == null) {
         this.songs.add(new Song(songTitle,songLength));
         return true;
         }
         return false;
+*/
     }
 
     public boolean addSongToPlaylist(int trackNumber, LinkedList<Song> playlist){
-        int index = trackNumber-1;
+//        int index = trackNumber-1;
+//
+//        if (index >= 0 && index <= this.songs.size()) {
+//            playlist.add(this.songs.get(index));
+//            return true;
+//        }
 
-        if (index >= 0 && index <= this.songs.size()) {
-            playlist.add(this.songs.get(index));
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if (checkedSong != null){
+            playlist.add(checkedSong);
             return true;
         }
+
         System.out.println("Track "+trackNumber+" not found in album.");
         return false;
     }
 
     public boolean addSongToPlaylist(String songTitle, LinkedList<Song> playlist){
-        Song checkedSong = findSong(songTitle);
+//        Song checkedSong = findSong(songTitle);
+//        if (checkedSong != null){
+//            playlist.add(checkedSong);
+//            return true;
+//        }
+
+        Song checkedSong = this.songs.findSong(songTitle);
         if (checkedSong != null){
             playlist.add(checkedSong);
             return true;
         }
+
         System.out.println("Song title "+songTitle+" was not found in this album.");
         return false;
     }
@@ -82,5 +101,5 @@ public class Album {
         }
 
     }
-//test
+
 }
